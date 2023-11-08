d55ab9735a1842fa126bfdfd84dd425d61ba992b
Angelo <angelo.pjsol@gmail.com>
2023-11-08 11:50:56 -0300

    code arch

--- tests/integration/hero.test.js
+++ tests/integration/hero.test.js
@@ -1,16 +1,17 @@
-import test from 'node:test'
+import test from "node:test"
 import assert from 'node:assert'
-import { promisify } from 'node:util'
+import { promisify } from "node:util"
+
 test('Hero Integration Test Suite', async (t) => {
   const testPort = 9009
-
-  // that's bad practice because it mutates the environment
   process.env.PORT = testPort
-  const { server } = await import('../../src/index.js')
-  
+
+  const {server} = await import('../../src/index.js')
+
   const testServerAddress = `http://localhost:${testPort}/heroes`
 
-  await t.test('it should create a hero', async (t) => {
+  
+  await t.test('it should create a hero', async(t) => {
     const data = {
       name: "Batman",
       age: 50,
@@ -18,10 +19,10 @@ test('Hero Integration Test Suite', async (t) => {
     }
 
     const request = await fetch(testServerAddress, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(data)
     })
-
+    
     assert.deepStrictEqual(
       request.headers.get('content-type'),
       'application/json'
@@ -33,17 +34,16 @@ test('Hero Integration Test Suite', async (t) => {
     assert.deepStrictEqual(
       result.success,
       'User created with success!!',
-      'it should return a valid text message'
+      'it should return a valid message'
     )
-      
+
     assert.ok(
       result.id.length > 30,
-      'id should be a valid uuid'
+      'it should be a valid uuid'
     )
 
 
-  })
+   })
 
   await promisify(server.close.bind(server))()
-
 })
